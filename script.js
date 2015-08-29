a9b11f5730f6ee175879d4f7cdb38eef03397ce9
Thiago Fonseca <tfonseca@ciandt.com>
2015-08-29 14:50:14 -0300

    Add some funcs

--- script.js
+++ script.js
@@ -3,22 +3,30 @@ var mycarModule = angular.module('MyCarModule', []);
 mycarModule.controller('SearchSitesController', function($scope){
 	$scope.searchSites = [
 		{
-			id: "100",
-			url: "buscaavancada/03"
+			id: 1,
+			url: "buscaavancada/100"
 		},
 		{
-			id: "200",
-			url: "buscaavancada/05"
+			id: 3,
+			url: "buscaavancada/200"
+		},
+		{
+			id: 2,
+			url: "buscaavancada/150"
 		}
 	];
 
 	$scope.add = function() {
 		$scope.searchSites.push($scope.searchSite);
+		$scope.searchSite = '';
 	};
 
 	$scope.remove = function(index) {
 		$scope.searchSites.splice(index, 1);
 	};
 
+	$scope.userName = 'Thiago Fonseca';
+	$scope.userPass = "admin123";
+	$scope.userPrice = 100;
 
-})
+});
