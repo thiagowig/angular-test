1b422c10d82992342b0c955b0986d722efc75b4a
Thiago Fonseca <dev.thiago@gmail.com>
2019-07-02 23:41:25 -0300

    Including a new angular app

--- ng-app/src/app/app.module.ts
+++ ng-app/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MoviesListComponent } from './components/movies-list/movies-list.component';
+import { CharactersListComponent } from './components/characters-list/characters-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesListComponent,
+    CharactersListComponent
   ],
   imports: [
     BrowserModule
