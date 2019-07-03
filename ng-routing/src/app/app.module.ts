1e17cb43190715009d0c02d295c3b403f9d8b844
Thiago Santos Fonseca <dev.thiago@gmail.com>
2019-07-03 11:38:50 -0300

    Implementing services

--- ng-routing/src/app/app.module.ts
+++ ng-routing/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { MoviesListComponent } from './components/movies-list/movies-list.component';
 import { CharactersListComponent } from './components/characters-list/characters-list.component';
+import { MovieComponent } from './components/movie/movie.component';
+import { CharacterComponent } from './components/character/character.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MoviesListComponent,
-    CharactersListComponent
+    CharactersListComponent,
+    MovieComponent,
+    CharacterComponent
   ],
   imports: [
     BrowserModule,
---
+++ ng-routing/src/app/classes/movie.ts
@@ -0,0 +1,10 @@
+export class Movie {
+
+    title: string;
+    episode_id: number;
+    opening_crawl: string;
+    director: string;
+    producer: string;
+    release_date: string;
+    
+}
